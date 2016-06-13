a13b7a49d03e3c48ef224ad1a6d00e2811433b90
ENDev15 <enduffy2014@outlook.com>
2016-06-13 16:54:19 -0700

    logger & data prep

--- init.lua
+++ init.lua
@@ -1,9 +1,21 @@
+-- global variables
 servertools = {}
-local modpath = minetest.get_modpath("servertools")
--- Load Modules
+st = servertools -- shortcut for servertools functions
+servertools.modname = minetest.get_current_modname() -- modname
+servertools.modpath = minetest.get_modpath(modname) -- modpath
+servetools.datapath = worldpath.."/"..minetest.get_worldpath(modname) -- path for data storage
+
+-- logger
+function servertools.log(content, log_type)
+  if log_type == nil then log_type = "action" end
+  minetest.log(log_type, "[ServerTools] "..content)
+end
+
+-- load modules
 dofile(modpath.."/functions.lua")
 dofile(modpath.."/privs.lua")
 dofile(modpath.."/genesis.lua")
 dofile(modpath.."/misc.lua")
+dofile(modpath.."/data.lua")
 
 minetest.log("action", "[ServerTools] Enabled Modules Loaded!") --print to log enabled modules loaded
